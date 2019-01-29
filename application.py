77ac2073b87efb0fe0342fdb5410bee0d8441caa
LeidenJar <hx.luan128@gmail.com>
2019-01-29 06:22:50 -0200

    Login centralizado

--- application.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route("/")
-def index():
-    variavel="LoucuraLoucuraLoucura"
-    return render_template("index.html", variavel=variavel)
