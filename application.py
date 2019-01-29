8a4705220484f3beb4c5ccad565528000d14f11a
LeidenJar <hx.luan128@gmail.com>
2019-01-29 00:11:37 -0200

    Explorando Flask

--- application.py
+++ application.py
@@ -1,7 +1,8 @@
-from flask import flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def index()
-    return "Hello, World!"
+def index():
+    variavel="LoucuraLoucuraLoucura"
+    return render_template("index.html", variavel=variavel)
