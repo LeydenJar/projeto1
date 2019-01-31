88367f5224441f960df0d78afe27426858b11a74
LeidenJar <hx.luan128@gmail.com>
2019-01-31 19:22:57 -0200

    Adicionado DB para usuarios e validação no registro e login

--- application.py
+++ application.py
@@ -1,5 +1,13 @@
-from flask import Flask, render_template
+import os
+
+from flask import Flask, render_template, request
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
 app = Flask(__name__)
+
+engine = create_engine("postgresql://postgres:123@localhost/projeto1db")
+db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 def home():
@@ -12,3 +20,63 @@ def login():
 @app.route("/registro")
 def registro():
     return render_template("registro.html")
+
+@app.route("/validar", methods=['POST'])
+def validar():
+
+    #Comparando e-mails #verificando se o email ja consta no db
+    email = request.form.get('e-mail')
+    cemail = request.form.get('confirmar e-mail')
+    vemail = email == cemail
+
+    emailv = db.execute("SELECT * FROM users WHERE email = :email", {"email": email}).fetchall()
+    if emailv == []:
+        emailv=True
+    else:
+        emailv = False
+
+
+    #verificando tamanho do usuario#     #verificando se o username ja existe no db
+    usuario = request.form.get("username")
+    tamanho_username = len(usuario)
+
+    userv=db.execute("SELECT * FROM users WHERE username = :user", {"user": usuario}).fetchall()
+    if userv == []:
+        userv=True
+    else:
+        userv=False
+
+
+    #verificando tamanho da senha#
+    senha = request.form.get("password")
+    tamanho_senha = len(senha)
+
+    #verificando se o usuario topou os termos#
+    topou = request.form.get('termos')
+
+    if vemail and tamanho_senha > 5 and tamanho_username > 5 and topou and userv and emailv:
+        db.execute("INSERT INTO users(username, password, email) VALUES (:user, :pass, :email)",
+        {"user":usuario, "pass":senha, "email":email})
+        db.commit()
+        return render_template("sucesso.html")
+    else:
+        return render_template("registro.html")
+
+
+@app.route("/validarl", methods=["POST"])
+def validarl():
+    user     = request.form.get("username")
+    password = request.form.get("password")
+    v = db.execute(
+        "SELECT * FROM users WHERE username = :user AND password = :pass",
+         {"user":user, "pass":password}
+                                            ).fetchall()
+    if v != []:
+        return render_template("layout2.html", retorno=v)
+    else:
+        return render_template("login.html")
+
+
+
+    if __name__ == "__main__":
+        app.run()
