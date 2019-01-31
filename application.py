c799c54db90617d44c9ed25119ba8ddeb6168658
LeidenJar <hx.luan128@gmail.com>
2019-01-31 21:12:15 -0200

    Eliminada a funçao 'validar'

--- application.py
+++ application.py
@@ -17,58 +17,57 @@ def home():
 def login():
     return render_template("login.html")
 
-@app.route("/registro")
+@app.route("/registro", methods=['POST', 'GET'])
 def registro():
-    return render_template("registro.html")
-
-@app.route("/validar", methods=['POST'])
-def validar():
-
-    #Comparando e-mails #verificando se o email ja consta no db
-    email = request.form.get('e-mail')
-    cemail = request.form.get('confirmar e-mail')
-    vemail = email == cemail
-
-    emailv = db.execute("SELECT * FROM users WHERE email = :email", {"email": email}).fetchall()
-    if emailv == []:
-        emailv=True
+    if request.method == 'GET':
+        return render_template("registro.html")
     else:
-        emailv = False
+        #Comparando e-mails #verificando se o email ja consta no db
+        email = request.form.get('e-mail')
+        cemail = request.form.get('confirmar e-mail')
+        vemail = email == cemail
+
+        emailv = db.execute("SELECT * FROM users WHERE email = :email", {"email": email}).fetchall()
+        if emailv == []:
+            emailv=True
+        else:
+            emailv = False
 
 
-    #verificando tamanho do usuario#     #verificando se o username ja existe no db
-    usuario = request.form.get("username")
-    tamanho_username = len(usuario)
+        #verificando tamanho do usuario#     #verificando se o username ja existe no db
+        usuario = request.form.get("username")
+        tamanho_username = len(usuario)
 
-    userv=db.execute("SELECT * FROM users WHERE username = :user", {"user": usuario}).fetchall()
-    if userv == []:
-        userv=True
-    else:
-        userv=False
+        userv=db.execute("SELECT * FROM users WHERE username = :user", {"user": usuario}).fetchall()
+        if userv == []:
+            userv=True
+        else:
+            userv=False
 
 
-    #verificando tamanho da senha#
-    senha = request.form.get("password")
-    tamanho_senha = len(senha)
+        #verificando tamanho da senha#
+        senha = request.form.get("password")
+        tamanho_senha = len(senha)
 
-    #verificando se o usuario topou os termos#
-    topou = request.form.get('termos')
+        #verificando se o usuario topou os termos#
+        topou = request.form.get('termos')
 
-    if vemail and tamanho_senha > 5 and tamanho_username > 5 and topou and userv and emailv:
-        db.execute("INSERT INTO users(username, password, email) VALUES (:user, :pass, :email)",
-        {"user":usuario, "pass":senha, "email":email})
-        db.commit()
-        return render_template("sucesso.html")
-    elif tamanho_senha<=5 or tamanho_username<=5:
-        return render_template("registro_erro.html", erro='erro: usuario e senha devem ter no minimo 6 caracteres')
-    elif not vemail:
-        return render_template("registro_erro.html", erro='erro: os e-mails digitados são diferentes')
-    elif not userv:
-        return render_template("registro_erro.html", erro='erro: O username ja existe, escolha outro')
-    elif not emailv:
-        return render_template("registro_erro.html", erro='erro: O email ja existe, escolha outro')
-    else:
-        return render_template("registro_erro.html", erro='erro: É necessário aceitar os termos')
+        if vemail and tamanho_senha > 5 and tamanho_username > 5 and topou and userv and emailv:
+            db.execute("INSERT INTO users(username, password, email) VALUES (:user, :pass, :email)",
+            {"user":usuario, "pass":senha, "email":email})
+            db.commit()
+            return render_template("sucesso.html")
+        elif tamanho_senha<=5 or tamanho_username<=5:
+            return render_template("registro_erro.html", erro='erro: usuario e senha devem ter no minimo 6 caracteres')
+        elif not vemail:
+            return render_template("registro_erro.html", erro='erro: os e-mails digitados são diferentes')
+        elif not userv:
+            return render_template("registro_erro.html", erro='erro: O username ja existe, escolha outro')
+        elif not emailv:
+            return render_template("registro_erro.html", erro='erro: O email ja existe, escolha outro')
+        else:
+            return render_template("registro_erro.html", erro='erro: É necessário aceitar os termos')
+
 
 @app.route("/validarl", methods=["POST"])
 def validarl():
