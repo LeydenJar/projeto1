d1f02f6302eeba3007a2c1b4b7760f8476dcfc51
LeydenJar <46806315+LeydenJar@users.noreply.github.com>
2019-08-28 15:58:52 -0300

    updated to fit heroku database

--- import.py
+++ import.py
@@ -2,8 +2,9 @@ import csv
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+import os
 
-engine = create_engine("postgresql://postgres:123@localhost/projeto1db")
+engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
 db.execute('CREATE TABLE books (isbn VARCHAR PRIMARY KEY, title VARCHAR, author VARCHAR, year INTEGER)')
